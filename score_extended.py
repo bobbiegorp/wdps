dadef3c381c16c35b7ecb7fbb74f5e9b285f1d1c
ML77777 <36936523+ML77777@users.noreply.github.com>
2019-12-04 13:38:02 +0100

    Add files via upload

---
+++ score_extended.py
@@ -0,0 +1,51 @@
+import sys
+
+gold_file = sys.argv[1]
+pred_file = sys.argv[2]
+
+# Load the gold standard
+gold = {}
+for line in open(gold_file):
+    record, string, entity = line.strip().split('\t', 2)
+    gold[(record, string)] = entity
+n_gold = len(gold)
+print('Gold entities: %s' % n_gold)
+
+# Load the predictions
+pred = {}
+for line in open(pred_file,encoding="utf-8"):
+    record, string, entity = line.strip().split('\t', 2)
+    pred[(record, string)] = entity
+n_predicted = len(pred)
+print('Linked entities: %s' % n_predicted)
+
+# Evaluate predictions
+n_correct = sum( int(pred[i]==gold[i]) for i in set(gold) & set(pred) )
+
+print("Amount of mappings: %s" % len(set(gold) & set(pred)))
+print('Correct mappings: %s' % n_correct)
+print("Amount of incorrect: mappings: %s" % (len(set(gold) & set(pred)) - n_correct))
+
+file = open("incorrect_mapping.txt","w+",encoding="utf-8")
+line = "(Doc,Word)" + '\t\t\t\t\t' + "Predictions" + '\t\t' + "Gold" + "\n"
+file.write(line)
+
+for i in (set(gold) & set(pred)):
+    if not int(pred[i]==gold[i]):
+        line = str(i) + '\t' + pred[i] + '\t' + gold[i] + "\n"
+        file.write(line)
+
+file = open("entities_not_recognized.txt","w+",encoding="utf-8")
+line = "(Doc,Word)" + "\n"
+file.write(line)
+for i in (set(gold) - (set(gold) & set(pred))):
+    line = str(i) + "\n" #+ '\t' + pred[i] + '\t' + gold[i] + "\n"
+    file.write(line)
+
+# Calculate scores
+precision = float(n_correct) / float(n_predicted)
+print('Precision: %s' % precision )
+recall = float(n_correct) / float(n_gold)
+print('Recall: %s' % recall )
+f1 = 2 * ( (precision * recall) / (precision + recall) )
+print('F1: %s' % f1 )
